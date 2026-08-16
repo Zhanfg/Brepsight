355f430bf43cdb77784a5ccff7c49c22f9d5183e
Axymorrsen <Axymosen@outlook.com>
2026-08-17 00:14:55 +0800

    feat(native): add STL importer interface

---
+++ packages/cad_engine/android/src/main/cpp/stl_importer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+#include "mesh_document.h"
+
+namespace brepsight {
+
+bool loadStl(const std::string& path, MeshData& out, std::string& error);
+
+}  // namespace brepsight
